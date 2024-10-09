b56b6f47c726e99e79693d58ebd795bc5232e25a
McKay Larman <lmlarman@byui.edu>
2024-10-08 22:24:09 -0600

    Cleaned up code style and added the youtube link to the readme.

--- Python Networking/Client.py
+++ Python Networking/Client.py
@@ -1,26 +1,43 @@
+# Part of python's standard libaray, it allows us to manage network connections
+# using sockets. 
 import socket
 
+'''
+client_program
+inputs: none
+outputs: none
+This code allows a client to connect to a server by providing the server's port
+number. The client and server can then exchange notes in a simple chat structure.
+'''
 def client_program():
     # Set up the host/port touple to connect to the socket.
     host = socket.gethostname()
-    port = int(input("Enter the Server's host number."))
+    port = int(input("Enter the Server's port number. "))
+
     # create socket object with default settings, then connect to the host.
     client_socket = socket.socket()
     client_socket.connect((host, port))
 
     print("Welcome to the chat!")
+    print("You can message back and forth as long as you would like")
+    print("Type 'bye' when you are done.")
+
     # Input for message.
     message = input(" -> ")
     # Communication loop, exit when you type 'Bye'.
     while message.lower().strip() != 'bye':
         # Send the message
         client_socket.send(message.encode())
+
         # Retrieve the message that was sent back
         data = client_socket.recv(1024).decode()
+
         # Print the message
         print('Received from server: ' + data)
+
         # Get a new item.
         message = input(" -> ")
+
     # Close the socket when you are done.
     client_socket.close()
 
